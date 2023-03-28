6dcb6e52d07a83b8ab5674f351dafeede9086f96
James Lyons <hello@jameslyons.design>
2023-03-27 20:27:18 -0700

    Update _app.js
    `
        ________  _____ ______   ________  ________
       |\   __  \|\   _ \  _   \|\   ___ \|\   __  \
       \ \  \|\  \ \  \\\__\ \  \ \  \_|\ \ \  \|\  \
        \ \   ____\ \  \\|__| \  \ \  \ \\ \ \   _  _\
         \ \  \___|\ \  \    \ \  \ \  \_\\ \ \  \\  \|
          \ \__\    \ \__\    \ \__\ \_______\ \__\\ _\
           \|__|     \|__|     \|__|\|_______|\|__|\|__|
      `

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,24 @@
+import React, { useEffect } from 'react';
 import '../styles/globals.css';
+
+function printAsciiArt() {
+  const asciiArt = `
+    ________  _____ ______   ________  ________
+   |\   __  \|\   _ \  _   \|\   ___ \|\   __  \
+   \ \  \|\  \ \  \\\__\ \  \ \  \_|\ \ \  \|\  \
+    \ \   ____\ \  \\|__| \  \ \  \ \\ \ \   _  _\
+     \ \  \___|\ \  \    \ \  \ \  \_\\ \ \  \\  \|
+      \ \__\    \ \__\    \ \__\ \_______\ \__\\ _\
+       \|__|     \|__|     \|__|\|_______|\|__|\|__|
+  `;
+  console.log(asciiArt);
+}
+
 
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    printAsciiArt();
+  }, []);
   return <Component {...pageProps} />;
 }
