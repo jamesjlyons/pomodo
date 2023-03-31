23836dc2c2f62fb8b20989c0411f3ce9c08a692c
James Lyons <hello@jameslyons.design>
2023-03-30 23:01:52 -0700

    icons and layout

--- components/Pomodoro.tsx
+++ components/Pomodoro.tsx
@@ -4,7 +4,7 @@ import * as Tone from 'tone';
 import NotificationControls from 'components/NotificationControls';
 import * as Switch from '@radix-ui/react-switch';
 import * as Toast from '@radix-ui/react-toast';
-
+import IconButton from './IconButton';
 
 export default function Pomodoro() {
   let timer = {
@@ -22,7 +22,7 @@ export default function Pomodoro() {
   const [seconds, setSeconds] = useState(0);
   const [pmdrCount, setpmdrCount] = useState(1);
   const [sessionType, setSessionType] = useState('work');
-  const [timerStart, setTimerStart] = useState(false);
+  const [timerRunning, setTimerRunning] = useState(false);
   const [sound, setSound] = useState(true);
   const [prevSessionType, setPrevSessionType] = useState('work');
   const [notifEnabled, setNotifEnabled] = useState<boolean>(false);
@@ -33,15 +33,15 @@ export default function Pomodoro() {
   const toastTimeRef = useRef(0);
 
   function handleStart() {
-    if (!timerStart) {
-      setTimerStart(true);
+    if (!timerRunning) {
+      setTimerRunning(true);
       timerWorkerRef.current?.postMessage({
         action: 'start',
         minutes: minutes,
         seconds: seconds,
       });
     } else {
-      setTimerStart(false);
+      setTimerRunning(false);
       timerWorkerRef.current?.postMessage({ action: 'pause' });
     }
   }
@@ -53,7 +53,6 @@ export default function Pomodoro() {
   const handleReset = () => {
     timerWorkerRef.current?.postMessage({ action: 'reset' });
   };
-
 
   const handleSubtract = () => {
     if (minutes > 0) {
@@ -74,9 +73,8 @@ export default function Pomodoro() {
   function spawnNotification(body: string, title: string) {
     if (notifEnabled) {
       const notification = new Notification(title, { body });
-    }
-    else {
-      return
+    } else {
+      return;
     }
   }
 
@@ -95,29 +93,29 @@ export default function Pomodoro() {
       console.log(event.code); // Add this line to log the events
 
       switch (event.code) {
-        case "Space":
+        case 'Space':
           handleStart();
-          if (timerStart) {
-            showToast("Timer Paused");
+          if (timerRunning) {
+            showToast('Timer Paused');
           } else {
-            showToast("Timer Started");
+            showToast('Timer Started');
           }
           break;
-        case "ArrowRight":
+        case 'ArrowRight':
           handleSkip();
-          showToast("Skipped");
+          showToast('Skipped');
           break;
-        case "ArrowLeft":
+        case 'ArrowLeft':
           handleReset();
-          showToast("Reset");
+          showToast('Reset');
           break;
-        case "ArrowUp":
+        case 'ArrowUp':
           handleAdd();
-          showToast("Minute Added");
+          showToast('Minute Added');
           break;
-        case "ArrowDown":
+        case 'ArrowDown':
           handleSubtract();
-          showToast("Minute Subtracted");
+          showToast('Minute Subtracted');
           break;
         default:
           break;
@@ -127,16 +125,19 @@ export default function Pomodoro() {
       return () => clearTimeout(toastTimeRef.current);
     };
 
-    window.addEventListener("keydown", handleKeyDown);
+    window.addEventListener('keydown', handleKeyDown);
 
     return () => {
-      window.removeEventListener("keydown", handleKeyDown);
+      window.removeEventListener('keydown', handleKeyDown);
     };
-
-  }, [handleStart, handleSkip, handleReset, handleAdd, handleSubtract, toastOpen]);
-
-
-
+  }, [
+    handleStart,
+    handleSkip,
+    handleReset,
+    handleAdd,
+    handleSubtract,
+    toastOpen,
+  ]);
 
   useEffect(() => {
     // sound and notificasion functions
@@ -182,7 +183,6 @@ export default function Pomodoro() {
       notifyForSessionType(sessionType);
       setPrevSessionType(sessionType);
     }
-
   }, [sound, sessionType, prevSessionType]);
 
   useEffect(() => {
@@ -201,11 +201,10 @@ export default function Pomodoro() {
         setSeconds(event.data.seconds);
         setpmdrCount(event.data.pmdrCount);
         setSessionType(event.data.sessionType);
-
       } else if (event.data.type === 'reset') {
         setMinutes(timer.pomodoro);
         setSeconds(0);
-        setTimerStart(false);
+        setTimerRunning(false);
         setpmdrCount(1);
       }
     };
@@ -214,7 +213,7 @@ export default function Pomodoro() {
     return () => {
       timerWorker.terminate();
     };
-  }, []); // Remove 'seconds' and 'timerStart' from the dependency array
+  }, []); // Remove 'seconds' and 'timerRunning' from the dependency array
 
   //   add 0 to minutes and seconds if less than 10
   const timerMinutes = minutes < 10 ? `0${minutes}` : minutes;
@@ -222,62 +221,292 @@ export default function Pomodoro() {
 
   return (
     <div className="pomodoro">
-      <h1 className="timer">
-        {timerMinutes}:{timerSeconds}
-        <span className="message" style={{ opacity: 0.3, marginTop: 16 }}>
-          {sessionType === 'work' && ' Work'}
-          {sessionType === 'shortBreak' && ' Break'}
-          {sessionType === 'longBreak' && ' Long Break'}
-        </span>
-      </h1>
-      <div className="controls">
-        <button onClick={handleStart}>Start/Pause</button>
-        <button onClick={handleSkip}>Skip</button>
-        <button onClick={handleReset}>Reset</button>
-        <button onClick={handleSubtract} disabled={minutes === 0 ? true : false}>-1</button>
-        <button onClick={handleAdd}>+1</button>
+      <div
+        className="container-inner"
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+          padding: '0px',
+          gap: '27px',
+          width: '430px',
+          height: '233px',
+          border: '1px solid rgba(211, 211, 211, 0.3)',
+          borderRadius: '2px',
+        }}
+      >
+        <div
+          className="session"
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            justifyContent: 'center',
+            padding: 0,
 
+            width: 316,
+            height: 113,
+          }}
+        >
+          <div
+            style={{
+              display: 'flex',
+              flexDirection: 'row',
+              justifyContent: 'center',
+              alignItems: 'center',
+              padding: '10px',
+              gap: '30px',
+            }}
+          >
+            <IconButton
+              onClick={handleSubtract}
+              disabled={minutes === 0 ? true : false}
+              title="Subtract"
+              icon={
+                <svg
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    clip-rule="evenodd"
+                    d="M6 12C6 11.4477 6.44772 11 7 11H17C17.5523 11 18 11.4477 18 12C18 12.5523 17.5523 13 17 13H7C6.44772 13 6 12.5523 6 12Z"
+                    fill="currentColor"
+                  />
+                </svg>
+              }
+            />
+
+            <h1
+              className="timer"
+              style={{
+                fontWeight: 700,
+                fontSize: 64,
+                letterSpacing: -1.43,
+                margin: 0,
+                fontFeatureSettings: `'tnum' on, 'lnum' on, 'ss01' on`,
+              }}
+            >
+              {timerMinutes}:{timerSeconds}
+            </h1>
+
+            <IconButton
+              onClick={handleAdd}
+              title="Add"
+              icon={
+                <svg
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    clip-rule="evenodd"
+                    d="M12 6C12.5523 6 13 6.44772 13 7V11H17C17.5523 11 18 11.4477 18 12C18 12.5523 17.5523 13 17 13H13V17C13 17.5523 12.5523 18 12 18C11.4477 18 11 17.5523 11 17V13H7C6.44772 13 6 12.5523 6 12C6 11.4477 6.44772 11 7 11H11V7C11 6.44772 11.4477 6 12 6Z"
+                    fill="currentColor"
+                  />
+                </svg>
+              }
+            />
+          </div>
+
+          <span
+            className="message"
+            style={{
+              fontFamily: 'Inter',
+              fontStyle: 'normal',
+              fontWeight: 400,
+              fontSize: 13,
+              /* identical to box height */
+
+              letterSpacing: -0.04,
+              fontFeatureSettings: `'tnum' on, 'lnum' on`,
+
+              opacity: 0.4,
+            }}
+          >
+            {sessionType === 'work' && ' Work'}
+            {sessionType === 'shortBreak' && ' Break'}
+            {sessionType === 'longBreak' && ' Long Break'}
+          </span>
+        </div>
+        <div
+          className="controls"
+          style={{
+            display: 'flex',
+            flexDirection: 'row',
+            justifyContent: 'center',
+            alignItems: 'center',
+            padding: '10px',
+            gap: '22px',
+            width: '126.4px',
+            height: '44px',
+          }}
+        >
+          <IconButton
+            onClick={handleReset}
+            title="Reset"
+            icon={
+              <svg
+                width="16"
+                height="15"
+                viewBox="0 0 16 15"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M3.49023 9.09971C4.14917 10.964 5.92715 12.2997 8.01711 12.2997C10.6681 12.2997 12.8171 10.1507 12.8171 7.49971C12.8171 4.84874 10.6681 2.69971 8.01711 2.69971C6.28691 2.69971 5.1624 3.48051 4.04663 4.79971M3.80011 2.69971V4.49971C3.80011 4.83108 4.06874 5.09971 4.40011 5.09971H6.20011"
+                  stroke="currentColor"
+                  stroke-width="1.2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                />
+              </svg>
+            }
+          />
+          <div
+            style={{
+              color: 'var(--icon)',
+            }}
+          >
+            <IconButton
+              onClick={handleStart}
+              title={timerRunning ? 'Pause' : 'Start'}
+              icon={
+                timerRunning ? (
+                  <svg
+                    width="25"
+                    height="25"
+                    viewBox="0 0 25 25"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M6.7002 3.5C5.31948 3.5 4.2002 4.61929 4.2002 6V19C4.2002 20.3807 5.31948 21.5 6.7002 21.5H7.7002C9.08091 21.5 10.2002 20.3807 10.2002 19V6C10.2002 4.61929 9.08091 3.5 7.7002 3.5H6.7002Z"
+                      fill="currentColor"
+                    />
+                    <path
+                      d="M16.7002 3.5C15.3195 3.5 14.2002 4.61929 14.2002 6V19C14.2002 20.3807 15.3195 21.5 16.7002 21.5H17.7002C19.0809 21.5 20.2002 20.3807 20.2002 19V6C20.2002 4.61929 19.0809 3.5 17.7002 3.5H16.7002Z"
+                      fill="currentColor"
+                    />
+                  </svg>
+                ) : (
+                  <svg
+                    width="24"
+                    height="25"
+                    viewBox="0 0 24 25"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <g clip-path="url(#clip0_402_101)">
+                      <path
+                        d="M21.7781 11.2156L8.27813 2.97497C8.05326 2.83247 7.79356 2.75448 7.52738 2.74952C7.26121 2.74456 6.99878 2.81282 6.76876 2.94684C6.53533 3.07491 6.34074 3.26353 6.20545 3.49285C6.07017 3.72217 5.9992 3.98372 6.00001 4.24997V20.75C5.9992 21.0162 6.07017 21.2778 6.20545 21.5071C6.34074 21.7364 6.53533 21.925 6.76876 22.0531C6.99878 22.1871 7.26121 22.2554 7.52738 22.2504C7.79356 22.2455 8.05326 22.1675 8.27813 22.025L21.7781 13.7843C21.9994 13.6509 22.1824 13.4625 22.3095 13.2374C22.4365 13.0124 22.5033 12.7584 22.5033 12.5C22.5033 12.2416 22.4365 11.9875 22.3095 11.7625C22.1824 11.5375 21.9994 11.3491 21.7781 11.2156Z"
+                        fill="currentColor"
+                      />
+                    </g>
+                    <defs>
+                      <clipPath id="clip0_402_101">
+                        <rect
+                          width="24"
+                          height="24"
+                          fill="currentColo"
+                          transform="translate(0 0.5)"
+                        />
+                      </clipPath>
+                    </defs>
+                  </svg>
+                )
+              }
+            />
+          </div>
+          <IconButton
+            onClick={handleSkip}
+            title="Skip"
+            icon={
+              <svg
+                width="25"
+                height="25"
+                viewBox="0 0 25 25"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M14.2002 16.5L17.1395 13.5607C17.7253 12.9749 17.7253 12.0251 17.1395 11.4393L14.2002 8.5M7.2002 16.5L10.1395 13.5607C10.7253 12.9749 10.7253 12.0251 10.1395 11.4393L7.2002 8.5"
+                  stroke="currentColor"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                />
+              </svg>
+            }
+          />
+        </div>
       </div>
-      <form style={{
-        fontSize: 13,
-        marginTop: 44,
-      }}>
+
+      <form
+        style={{
+          fontSize: 13,
+          marginTop: 44,
+        }}
+      >
         <h4 style={{ fontSize: 12, opacity: 0.5 }}>Settings</h4>
         <div style={{ display: 'flex', alignItems: 'center' }}>
-          <label className="Label" htmlFor="sound" style={{ opacity: 0.5, paddingRight: 16 }}>
+          <label
+            className="Label"
+            htmlFor="sound"
+            style={{ opacity: 0.5, paddingRight: 16 }}
+          >
             Sound
           </label>
-          <Switch.Root className="SwitchRoot" id="sound" checked={sound} onCheckedChange={() => setSound(!sound)}>
+          <Switch.Root
+            className="SwitchRoot"
+            id="sound"
+            checked={sound}
+            onCheckedChange={() => setSound(!sound)}
+          >
             <Switch.Thumb className="SwitchThumb" />
           </Switch.Root>
         </div>
       </form>
 
-
-      <NotificationControls notifEnabled={notifEnabled}
-        setNotifEnabled={setNotifEnabled} />
+      <NotificationControls
+        notifEnabled={notifEnabled}
+        setNotifEnabled={setNotifEnabled}
+      />
       {/* <div
         className="pmdrCount"
         style={{ fontSize: 12, opacity: 0.5, marginTop: 16 }}
       >
-        running: {timerStart && 'yes'}
-        {!timerStart && 'no'}, pmdrCount: {pmdrCount}
+        running: {timerRunning && 'yes'}
+        {!timerRunning && 'no'}, pmdrCount: {pmdrCount}
       </div> */}
       <div
         className="shortcuts"
         style={{ fontSize: 12, opacity: 0.5, marginTop: 48, lineHeight: 1.6 }}
       >
         <h4 style={{ marginBottom: -8 }}>Keyboard shortcuts</h4>
-        <p>Start/Pause: space  <br />
-          Skip: → <br /> Add minute: ↑ <br /> Subtract minute: ↓</p>
+        <p>
+          Start/Pause: space <br />
+          Skip: → <br /> Add minute: ↑ <br /> Subtract minute: ↓
+        </p>
       </div>
 
-      <Toast.Provider duration={1000} >
-        <Toast.Root className="ToastRoot" open={toastOpen} onOpenChange={setToastOpen}>
+      <Toast.Provider duration={1000}>
+        <Toast.Root
+          className="ToastRoot"
+          open={toastOpen}
+          onOpenChange={setToastOpen}
+        >
           <Toast.Title>{toastContent}</Toast.Title>
         </Toast.Root>
         <Toast.Viewport />
       </Toast.Provider>
-    </div >
+    </div>
   );
 }
