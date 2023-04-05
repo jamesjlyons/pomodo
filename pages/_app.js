837606684708115341909da162abe55b4bd844a1
James Lyons <hello@jameslyons.design>
2023-04-04 21:19:55 -0700

    fix try 2

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { Inter } from '@next/font/google'
+import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 import '../styles/globals.css';
 
