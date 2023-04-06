4ad5e172d51dbfc7ac0cd9f081f70b9ec188fa1f
James Lyons <hello@jameslyons.design>
2023-04-05 22:09:57 -0700

    Update IconButton.tsx

--- components/IconButton.tsx
+++ components/IconButton.tsx
@@ -31,6 +31,7 @@ const IconButton: React.FC<IconButtonProps> = ({
         cursor: disabled ? 'not-allowed' : 'pointer',
         padding: 8,
         opacity: disabled ? 0.5 : 1,
+          color: unset,
       }}
       disabled={disabled}
     >
