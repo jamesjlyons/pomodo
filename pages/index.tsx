fc1f5b77146b37ba28a5a4b323beaed1127cf310
James Lyons <hello@jameslyons.design>
2023-06-14 22:16:32 -0700

    meta and icon updates

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,50 +10,50 @@ function HomePage() {
   return (
     <div className="App">
       <Head>
-        <title>Pomodo</title>
+        <title>Pomodoro</title>
         {/* <meta content="width=device-width, initial-scale=1" name="viewport" /> */}
         <meta
           name='viewport'
           content='minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover'
         />
         <meta name="description" content="⏲️" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/icons/favicon.ico" />
 
-        <meta name="application-name" content="Pomodo" />
+        <meta name="application-name" content="Pomodoro" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="default" />
-        <meta name="apple-mobile-web-app-title" content="Pomodo" />
+        <meta name="apple-mobile-web-app-title" content="Pomodoro" />
         <meta name="description" content="Pomdoro timer" />
         <meta name="format-detection" content="telephone=no" />
         <meta name="mobile-web-app-capable" content="yes" />
         <meta name="msapplication-config" content="/icons/browserconfig.xml" />
         <meta name="msapplication-TileColor" content="#2B5797" />
         <meta name="msapplication-tap-highlight" content="no" />
-        <meta name="theme-color" content="#000000" />
-        {/*
-        <link rel="apple-touch-icon" href="/icons/touch-icon-iphone.png" />
-        <link rel="apple-touch-icon" sizes="152x152" href="/icons/touch-icon-ipad.png" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/icons/touch-icon-iphone-retina.png" />
-        <link rel="apple-touch-icon" sizes="167x167" href="/icons/touch-icon-ipad-retina.png" />
+        <meta name="theme-color" content="#ffffff" media="(prefers-color-scheme: light)" />
+        <meta name="theme-color" content="#000000" media="(prefers-color-scheme: dark)" />
 
-        <link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16x16.png" />
+        <link rel="apple-touch-icon" href="/icons/72.png" />
+        <link rel="apple-touch-icon" sizes="152x152" href="/icons/152.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/icons/180.png" />
+        <link rel="apple-touch-icon" sizes="167x167" href="/icons/167.png" />
+
+        <link rel="icon" type="image/png" sizes="32x32" href="/icons/32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/icons/16.png" />
         <link rel="manifest" href="/manifest.json" />
-        <link rel="mask-icon" href="/icons/safari-pinned-tab.svg" color="#5bbad5" />
-        <link rel="shortcut icon" href="/favicon.ico" />
+        <link rel="shortcut icon" href="/icons/favicon.ico" />
 
-        <meta name="twitter:card" content="summary" />
+        {/* <meta name="twitter:card" content="summary" />
         <meta name="twitter:url" content="https://yourdomain.com" />
         <meta name="twitter:title" content="PWA App" />
         <meta name="twitter:description" content="Best PWA App in the world" />
         <meta name="twitter:image" content="https://yourdomain.com/icons/android-chrome-192x192.png" />
-        <meta name="twitter:creator" content="@DavidWShadow" />
+        <meta name="twitter:creator" content="@DavidWShadow" /> */}
         <meta property="og:type" content="website" />
-        <meta property="og:title" content="PWA App" />
-        <meta property="og:description" content="Best PWA App in the world" />
-        <meta property="og:site_name" content="PWA App" />
-        <meta property="og:url" content="https://yourdomain.com" />
-        <meta property="og:image" content="https://yourdomain.com/icons/apple-touch-icon.png" /> */}
+        <meta property="og:title" content="Pomodoro" />
+        <meta property="og:description" content="A performant pomodoro timer for the web" />
+        <meta property="og:site_name" content="Pomodoro" />
+        <meta property="og:url" content="https://pomodoro.jameslyons.design/" />
+        <meta property="og:image" content="https://pomodoro.jameslyons.design/icons/apple-touch-icon.png" />
 
       </Head>
       {/* <Script src="/timer-worker.js" strategy="worker" /> */}
