56b6c3f3943e5ef64bc29f959731fa79fdee4007
James Lyons <hello@jameslyons.design>
2024-10-14 12:38:38 -0700

    add openpanel

--- pages/_app.js
+++ pages/_app.js
@@ -2,8 +2,9 @@ import React, { useEffect } from 'react';
 import { ThemeProvider } from 'next-themes';
 import '../styles/globals.css';
 // import PlausibleProvider from 'next-plausible';
-import { Analytics } from '@vercel/analytics/react';
+// import { Analytics } from '@vercel/analytics/react';
 // import { Toaster, toast } from 'sonner';
+import { OpenPanelComponent } from '@openpanel/nextjs';
 
 function printAsciiArt() {
   const asciiArt = `
@@ -21,10 +22,18 @@ export default function MyApp({ Component, pageProps }) {
   }, []);
   return (
     <ThemeProvider themes={['light', 'dark', 'arc']} defaultTheme="dark">
+      <OpenPanelComponent
+        clientId="6c32975d-7f33-468b-b684-8024cb40a3f8"
+        trackScreenViews={true}
+        trackAttributes={true}
+        trackOutgoingLinks={true}
+        // If you have a user id, you can pass it here to identify the user
+        // profileId={'123'}
+      />
       {/* <PlausibleProvider domain="pomodoro.jameslyons.design"> */}
       {/* <Toaster /> */}
       <Component {...pageProps} />
-      <Analytics />
+      {/* <Analytics /> */}
       {/* </PlausibleProvider> */}
     </ThemeProvider>
   );
